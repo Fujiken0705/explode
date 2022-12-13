9d499f4aee03605eeb4b320be3a27ed857ad2b4d
Fujiken0705 <kenfujita75@gmail.com>
2022-12-14 01:55:49 +0900

    乱数追加

--- explode!/ViewController.swift
+++ explode!/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var explodecount = 0
+    var explodefunc = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var bombImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countLabel.text = "0"
+        bombImage.image = #imageLiteral(resourceName: "bakudan")
     }
 
-
+    @IBAction func pushButton(_ sender: UIButton) {
+        
+        explodefunc = Int.random(in:0...19)
+        
+        if explodefunc == 7{
+            bombImage.image = #imageLiteral(resourceName: "bakuha")
+            countLabel.text = "GAMEOVER"
+            
+        }
+        else{
+            explodecount += 1
+            let  counter = String( explodecount )
+            countLabel.text = counter
+            
+        }
+    }
+    
+    
 }
-
